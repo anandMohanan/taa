b7f443db19e36313561e8deff7d9d2e4ef4ae659
anandMohanan <ananthkvmohanan@gmail.com>
2022-03-10 17:44:21 +0530

    added like count to the users who are not logged in yet correctly

--- components/like.js
+++ components/like.js
@@ -66,26 +66,28 @@ export const Like = ({ user, post }) => {
       </button>
     )
   ) : (
-    <Link href="./login">
-      <a className="text-text-secondary">
-        {" "}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M7 11l5-5m0 0l5 5m-5-5v12"
-          />
-        </svg>
-        {post.likeCount}
-      </a>
-    </Link>
+    <>
+      <Link href="./login">
+        <a className="text-text-secondary">
+          {" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M7 11l5-5m0 0l5 5m-5-5v12"
+            />
+          </svg>
+        </a>
+      </Link>
+      <span className="text-text-secondary p-2">{post.likeCount}</span>
+    </>
   );
   return (
     // <button className="text-text-secondary p-3">
